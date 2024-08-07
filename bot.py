c2bd159e114ec723e22d30f034347e765745faba
mason131928 <mason131928@gmail.com>
2024-08-07 15:19:36 +0800

    更新 bot.py 文件中英文icon

--- bot.py
+++ bot.py
@@ -253,8 +253,8 @@ async def reset(update: Update, context):
 async def show_language_menu(update: Update, context):
     keyboard = [
         [
-            InlineKeyboardButton("-中文-", callback_data='lang_zh'),
-            InlineKeyboardButton("-English-", callback_data='lang_en')
+            InlineKeyboardButton("👉中文👈", callback_data='lang_zh'),
+            InlineKeyboardButton("👉English👈", callback_data='lang_en')
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
